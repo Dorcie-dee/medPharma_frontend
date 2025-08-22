e3cdbbb7caf1c486ee9121bf181def37a2d4f8af
Dorcie-dee <dorcasnquaye28@gmail.com>
2025-08-22 12:52:40 +0000

    live url added

--- src/BookingForm.jsx
+++ src/BookingForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-const API_BASE = "http://localhost:6002";   // my backend
+const API_BASE = "https://medpharma-care-backend.onrender.com/";   // my backend
 
 export default function BookingForm() {
   const [patientName, setPatientName] = useState("");
--- src/StatusView.jsx
+++ src/StatusView.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
 import axios from "axios";
 
-const API_BASE = "http://localhost:6002";   // my backend
+const API_BASE = "https://medpharma-care-backend.onrender.com/";   // my backend
 const socket = io(API_BASE);
 
 export default function StatusView() {
